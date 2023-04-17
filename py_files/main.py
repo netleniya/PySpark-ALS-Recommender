ed645a1968245c10a078905694abee532f77626c
Peter Fiee <peterfiee@icloud.com>
2023-04-17 12:41:05 -0400

    reformat output tables

--- py_files/main.py
+++ py_files/main.py
@@ -10,12 +10,14 @@ database = pd.read_parquet(os.path.join("outputs", "work_df")).reset_index(drop=
 
 def recommend_books_to_readers(user_id: int) -> pd.DataFrame:
     df = database.merge(user_recs, on=["bookId"], how="left")
-    return df[df["targetId"] == user_id][["title", "isbn13", "language"]]
+    print(f"Book recommendations for user with id {user_id}:")
+    return df[df["targetId"] == user_id][["title", "isbn13", "language"]].drop_duplicates(keep='first')
 
 
 def recommend_readers_for_book(isbn: int) -> pd.DataFrame:
     df = database[database["isbn13"] == isbn].merge(book_recs, on=["bookId"]).drop_duplicates(["targetId"])
-    return df[["bookId", "title", "targetId"]].set_index(["bookId", "title"])
+    print("Recommend: ",df.title.drop_duplicates().to_string(index=False, header=False), "to the following users")
+    return df[["targetId"]]
 
 
 def main() -> None:
