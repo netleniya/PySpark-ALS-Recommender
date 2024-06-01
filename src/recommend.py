11a252ea7f70b78b79814568f42c50c3852dcd0b
Peter Fiee <peterfiee@icloud.com>
2024-06-01 02:18:45 -0400

    Rearrange project structure and cleanup redundant files

---
+++ src/recommend.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+from pandas import DataFrame
+from abc import ABC, abstractmethod
+
+
+class Recommender(ABC):
+    def __init__(self, database: DataFrame) -> None:
+        self.database = database
+
+    @abstractmethod
+    def recommend(self, id: int) -> DataFrame: ...
+
+
+class BookRecommender(Recommender):
+    def __init__(self, database: DataFrame, user_recs: DataFrame) -> None:
+        super().__init__(database)
+        self.user_recs = user_recs
+
+    def recommend(self, id: int) -> DataFrame:
+        df = self.database.merge(self.user_recs, on=["bookId"], how="left")
+        print(f"Book recommendations for user with id {id}:")
+        return df[df["targetId"] == id][
+            ["title", "isbn13", "language", "image"]
+        ].drop_duplicates(keep="first")
+
+
+class ReaderRecommender(Recommender):
+    def __init__(self, database: DataFrame, book_recs: DataFrame) -> None:
+        super().__init__(database)
+        self.book_recs = book_recs
+
+    def recommend(self, isbn: int) -> DataFrame:
+        df = (
+            self.database[self.database["isbn13"] == isbn]
+            .merge(self.book_recs, on=["bookId"])
+            .drop_duplicates(["targetId"])
+        )
+        print(
+            "Recommend: ",
+            df.title.drop_duplicates().to_string(index=False, header=False),  # type: ignore
+            "to the following users",
+        )
+        return df[["targetId"]]
