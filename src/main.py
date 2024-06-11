f761c7a27f9a7b095d1ffc8e45196153b754d68c
Peter Fiee <peterfiee@icloud.com>
2024-06-10 23:23:53 -0400

    save progress

--- src/main.py
+++ src/main.py
@@ -1,11 +1,20 @@
-from recommend import BookRecommender, ReaderRecommender
+import pandas as pd
+
+from recommend import BookRecommender, UserRecommender
+from generate import BooksDataFrameGenerator
 from pathlib import Path
-import streamlit as st
 
 
 def main() -> None:
-    st.title(":book: Book Recommendations")
-    st.write("This is a demo of the recommendations app.")
+
+    book_libray = Path().cwd().parent.joinpath("data", "processed", "clean_df")
+    book_libray = pd.read_parquet(book_libray)
+
+    user_recs = UserRecommender().recommend(num_recommendations=10)
+
+    book_gen = BooksDataFrameGenerator(database=book_libray, user_recs=user_recs)
+
+    print(book_gen.generate_dataframe(user_id=5291))
 
 
 if __name__ == "__main__":
--- src/recommend.py
+++ src/recommend.py
@@ -1,45 +1,65 @@
-from pandas import DataFrame
 from abc import ABC, abstractmethod
+from pandas import DataFrame
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALSModel
 
 
 class Recommender(ABC):
-    def __init__(self, database: DataFrame) -> None:
-        self.database = database
+    spark = (
+        SparkSession.builder.appName("Group7 Recommender")
+        .config("spark.driver.memory", "8g")
+        .getOrCreate()
+    )
+
+    model = ALSModel.load("../models/recommender.model")
+
+    def __init__(self, local_model: ALSModel, spark_session: SparkSession) -> None:
+        self.model = local_model
+        self.spark = spark_session
 
     @abstractmethod
-    def generate_recommendations(self, model) -> None: ...
-
-    @abstractmethod
-    def recommend(self, id: int) -> DataFrame: ...
+    def recommend(self, num_recommendations: int) -> DataFrame: ...
 
 
 class BookRecommender(Recommender):
-    def __init__(self, database: DataFrame, user_recs) -> None:
-        super().__init__(database)
-        self.user_recs = user_recs
 
-    def recommend(self, id: int) -> DataFrame:
-        df = self.database.merge(self.user_recs, on=["bookId"], how="left")
-        print(f"Book recommendations for user with id {id}:")
-        return df[df["targetId"] == id][
-            ["title", "isbn13", "language", "image"]
-        ].drop_duplicates(keep="first")
+    def __init__(self) -> None:
+        super().__init__(local_model=self.model, spark_session=self.spark)
+
+    def recommend(self, num_recommendations: int) -> DataFrame:
+        user_recs = self.model.recommendForAllUsers(num_recommendations)
+        user_recs.createTempView("ALS_recs_temp")
+
+        query = """
+        SELECT
+            userId AS targetId,
+            bookIds_and_ratings.bookId,
+            bookIds_and_ratings.rating AS prediction
+        FROM ALS_recs_temp
+        LATERAL VIEW explode(recommendations) exploded_table
+        AS bookIds_and_ratings
+        """
+        clean_recs = self.spark.sql(query)
+        return clean_recs
 
 
-class ReaderRecommender(Recommender):
-    def __init__(self, database: DataFrame, book_recs) -> None:
-        super().__init__(database)
-        self.book_recs = book_recs
+class UserRecommender(Recommender):
 
-    def recommend(self, isbn: int) -> DataFrame:
-        df = (
-            self.database[self.database["isbn13"] == isbn]
-            .merge(self.book_recs, on=["bookId"])
-            .drop_duplicates(["targetId"])
-        )
-        print(
-            "Recommend: ",
-            df.title.drop_duplicates().to_string(index=False, header=False),  # type: ignore
-            "to the following users",
-        )
-        return df[["targetId"]]
+    def __init__(self) -> None:
+        super().__init__(local_model=self.model, spark_session=self.spark)
+
+    def recommend(self, num_recommendations: int) -> DataFrame:
+        book_recs = self.model.recommendForAllItems(num_recommendations)
+        book_recs.createTempView("ALS_books_temp")
+
+        query = """
+        SELECT
+            bookId AS bookId,
+            bookIds_and_userIds.userId AS targetId,
+            bookIds_and_userIds.rating AS predicted_rating
+        FROM ALS_books_temp
+        LATERAL VIEW explode(recommendations) exploded_table
+        AS bookIds_and_userIds
+        """
+        clean_recs = self.spark.sql(query)
+        return clean_recs.toPandas()
