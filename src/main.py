88b272f434b6d33c46b4957a80dfda248192e0c8
Peter Fiee <peterfiee@icloud.com>
2024-06-11 00:15:06 -0400

    streamlit skeleton

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import streamlit as st
 
 from recommend import BookRecommender, UserRecommender
 from generate import BooksDataFrameGenerator
@@ -7,14 +8,37 @@ from pathlib import Path
 
 def main() -> None:
 
-    book_libray = Path().cwd().parent.joinpath("data", "processed", "clean_df")
-    book_libray = pd.read_parquet(book_libray)
+    st.title(":book: Book Recommendation Engine")
+    st.header("Recommender")
 
-    user_recs = UserRecommender().recommend(num_recommendations=10)
+    @st.cache_data
+    def load_data():
+        biblio = Path().cwd().parent.joinpath("data", "processed", "clean_df")
+        biblio = pd.read_parquet(biblio)
+        return biblio
 
-    book_gen = BooksDataFrameGenerator(database=book_libray, user_recs=user_recs)
+    book_libray = load_data()
 
-    print(book_gen.generate_dataframe(user_id=5291))
+    if st.sidebar.checkbox("Show Dataframe", False):
+        st.dataframe(book_libray)
+
+    with st.sidebar:
+
+        users, books = st.tabs(["Users", "Books"])
+
+        with users:
+            user_id = st.selectbox("Select User", book_libray["userId"].unique())
+            num_recommendations = st.slider(
+                "Number of Books to Recommend", min_value=1, max_value=10, value=5
+            )
+
+        with books:
+            book_id = st.selectbox("Select Book", book_libray["bookId"].unique())
+            num_recommendations = st.slider(
+                "Recommend this book to how many users?", min_value=1, max_value=10, value=5
+            )
+
+
 
 
 if __name__ == "__main__":
