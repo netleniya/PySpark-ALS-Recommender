bda87c25ac12a3d44ec614f96fd1f7c67adc7641
Peter Fiee <peterfiee@icloud.com>
2024-06-11 06:19:02 -0400

    delete buggy streamlit in favor of gradio

--- src/main.py
+++ src/main.py
@@ -1,44 +1,80 @@
 import pandas as pd
-import streamlit as st
+import gradio as gr
 
 from recommend import BookRecommender, UserRecommender
-from generate import BooksDataFrameGenerator
+from generate import GenerateBookList, GenerateUsersList
 from pathlib import Path
+
+
+def load_data() -> pd.DataFrame:
+    biblio = Path(__file__).parents[1].joinpath("data", "processed", "clean_df")
+    biblio = pd.read_parquet(biblio)
+    return biblio
+
+
+book_libray = load_data()
+
+
+def get_book_recommendations(user_id, num_recs) -> pd.DataFrame:
+
+    try:
+        rec_obj = BookRecommender().recommend(num_recommendations=num_recs)
+        gen_obj = GenerateBookList(book_libray, rec_obj).generate_dataframe(
+            user_id=user_id
+        )
+        return gen_obj
+    except ValueError as e:
+        print(f"No recommendations for {user_id} or User not found")
+
+
+def get_user_recommendations(book_id, num_recs) -> pd.DataFrame:
+
+    try:
+        rec_obj = UserRecommender().recommend(num_recommendations=num_recs)
+        gen_obj = GenerateUsersList(book_libray, rec_obj).generate_dataframe(
+            isbn=book_id
+        )
+        return gen_obj
+    except ValueError as e:
+        print(f"No recommendations for {book_id} or Book not found")
 
 
 def main() -> None:
 
-    st.title(":book: Book Recommendation Engine")
-    st.header("Recommender")
+    book_rec = gr.Interface(
+        fn=get_book_recommendations,
+        inputs=[
+            gr.Number(label="User ID"),
+            gr.Number(label="Number of recommendations"),
+        ],
+        outputs=[
+            gr.DataFrame(
+                label="Recommended Books",
+                col_count=(3, "fixed"),
+            ),
+        ],
+        title="Book Recommender",
+    )
 
-    @st.cache_data
-    def load_data():
-        biblio = Path().cwd().parent.joinpath("data", "processed", "clean_df")
-        biblio = pd.read_parquet(biblio)
-        return biblio
+    user_rec = gr.Interface(
+        fn=get_user_recommendations,
+        inputs=[
+            gr.Number(label="Book ID"),
+            gr.Number(label="Number of recommendations"),
+        ],
+        outputs=[
+            gr.DataFrame(
+                label="Recommended Users",
+                col_count=(3, "fixed"),
+            ),
+        ],
+        title="User Recommender",
+    )
 
-    book_libray = load_data()
-
-    if st.sidebar.checkbox("Show Dataframe", False):
-        st.dataframe(book_libray)
-
-    with st.sidebar:
-
-        users, books = st.tabs(["Users", "Books"])
-
-        with users:
-            user_id = st.selectbox("Select User", book_libray["userId"].unique())
-            num_recommendations = st.slider(
-                "Number of Books to Recommend", min_value=1, max_value=10, value=5
-            )
-
-        with books:
-            book_id = st.selectbox("Select Book", book_libray["bookId"].unique())
-            num_recommendations = st.slider(
-                "Recommend this book to how many users?", min_value=1, max_value=10, value=5
-            )
-
-
+    interface = gr.TabbedInterface(
+        [book_rec, user_rec], ["Book Recommender", "User Recommender"]
+    )
+    interface.launch()
 
 
 if __name__ == "__main__":
